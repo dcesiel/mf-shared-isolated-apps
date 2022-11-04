a51dad8fca5825689c1a2bad57297355f944d363
David Cesiel <dcesiel@umich.edu>
2022-11-04 14:05:46 -0500

    Add files via upload

--- app1/src/bootstrap.js
+++ app1/src/bootstrap.js
@@ -3,5 +3,5 @@
 import isPositive from 'is-positive';
 
 console.log("App1 Initializing");
-console.log("App1", isPositive(num))
+console.log("App1", isPositive(5))
 window.app1SharedScope = __webpack_share_scopes__.default;
